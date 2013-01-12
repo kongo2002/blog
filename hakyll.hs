70c8692ebd7d7c161934c35e2274964cd5eb80d8
kongo2002 <kongo2002@googlemail.com>
2013-01-12 13:46:04 +0100

    use chronological instead
    sortByBaseName is deprecated

--- hakyll.hs
+++ hakyll.hs
@@ -49,7 +49,7 @@ main = hakyll $ do
     match "index.html" $ route idRoute
     create "index.html" $ constA mempty
         >>> arr (setField "title" "Recent posts")
-        >>> requireAllA "posts/*" (id *** arr (take 10 . reverse . sortByBaseName) >>> addPostList)
+        >>> requireAllA "posts/*" (id *** arr (take 10 . reverse . chronological) >>> addPostList)
         >>> applyTemplateCompiler "templates/index.html"
         >>> applyTemplateCompiler "templates/default.html"
         >>> relativizeUrlsCompiler
@@ -76,7 +76,7 @@ main = hakyll $ do
     match "posts.html" $ route idRoute
     create "posts.html" $ constA mempty
         >>> arr (setField "title" "All posts")
-        >>> requireAllA "posts/*" (id *** arr (reverse . sortByBaseName) >>> addPostList)
+        >>> requireAllA "posts/*" (id *** arr (reverse . chronological) >>> addPostList)
         >>> applyTemplateCompiler "templates/posts.html"
         >>> applyTemplateCompiler "templates/default.html"
         >>> relativizeUrlsCompiler
@@ -99,7 +99,7 @@ main = hakyll $ do
 
 addPostList :: Compiler (Page String, [Page String]) (Page String)
 addPostList = setFieldA "posts" $
-    arr (reverse . sortByBaseName)
+    arr (reverse . chronological)
         >>> require "templates/postitem.html" (\p t -> map (applyTemplate t) p)
         >>> arr mconcat
         >>> arr pageBody
